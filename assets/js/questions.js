2c97b9cd41c8cfe4dae6c10749bc1cb6de09fafd
Filip Kotanski <filip.kotanski@gmail.com>
2024-01-01 16:50:30 +0000

    Added js folder with questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,51 @@
+const quizData = [
+    {
+      question: "What is an event in JavaScript?",
+      choices: ["A. A type of function", "B. A user action or system occurrence that triggers a callback function", "C. A variable declaration"],
+      correctAnswer: "B. A user action or system occurrence that triggers a callback function"
+    },
+    {
+      question: "How do you prevent the default behavior of an event in JavaScript?",
+      choices: ["A. event.default();", "B. event.cancel();", "C. event.preventDefault();"],
+      correctAnswer: "C. event.preventDefault();"
+    },
+    {
+      question: "What is the purpose of `localStorage` in JavaScript?",
+      choices: ["A. To store data on the server", "B. To store data locally on the user's browser", "C. To store data in a database"],
+      correctAnswer: "B. To store data locally on the user's browser"
+    },
+    {
+      question: "What data type can be stored in `localStorage`?",
+      choices: ["A. Only strings", "B. Numbers and strings", "C. Any valid JavaScript data type"],
+      correctAnswer: "A. Only strings"
+    },
+    {
+      question: "How do you select an element with the id 'example' using JavaScript?",
+      choices: ["A. getElementById('example');", "B. selectElement('example');", "C. querySelector('example');"],
+      correctAnswer: "A. getElementById('example');"
+    },
+    {
+      question: "What does the `innerHTML` property do in the DOM?",
+      choices: ["A. Retrieves the HTML content of an element", "B. Sets the HTML content of an element", "C. Deletes an element from the DOM"],
+      correctAnswer: "B. Sets the HTML content of an element"
+    },
+    {
+      question: "What does API stand for in the context of web development?",
+      choices: ["A. Advanced Programming Interface", "B. Application Programming Interface", "C. Automated Programming Interface"],
+      correctAnswer: "B. Application Programming Interface"
+    },
+    {
+      question: "How do you make an API request in JavaScript using the Fetch API?",
+      choices: ["A. fetchRequest('https://api.example.com');", "B. request('https://api.example.com');", "C. fetch('https://api.example.com');"],
+      correctAnswer: "C. fetch('https://api.example.com');"
+    },
+    {
+        question: "What is event bubbling in JavaScript?",
+        choices: [
+          "A. A process where events propagate from the target element to its ancestors",
+          "B. A method to cancel event propagation",
+          "C. A way to capture events before they reach the target element"
+        ],
+        correctAnswer: "A. A process where events propagate from the target element to its ancestors"
+    }
+  ];
